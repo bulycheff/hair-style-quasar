1458f428a09d8c1c128738e127395a92a78204e0
Sergey Bulychev <bulycheff@ya.ru>
2022-05-19 18:12:44 +0500

    добавлен стор с ценами

--- src/stores/priceListStore.js
+++ src/stores/priceListStore.js
@@ -1,7 +1,22 @@
 import { defineStore } from 'pinia';
+import axios from 'axios';
 
 export const usePriceListStore = defineStore('priceListStore', {
-  state: () => ({}),
-  getters: {},
-  actions: {},
+  state: () => ({
+    matrix: [],
+    davines: [],
+    schwarzkopf: [],
+  }),
+  getters: {
+    loaded: (state) => state.matrix.length > 0
+      && state.davines.length > 0
+      && state.schwarzkopf.length > 0,
+  },
+  actions: {
+    async fill() {
+      this.matrix = (await axios.get('https://api.hair.site.net.ru/api/v1/price/matrix')).data;
+      this.davines = (await axios.get('https://api.hair.site.net.ru/api/v1/price/davines')).data;
+      this.schwarzkopf = (await axios.get('https://api.hair.site.net.ru/api/v1/price/schwarzkopf')).data;
+    },
+  },
 });
